ec425e1d25363cf8935448ccbde0558108c39da9
stephannn <38951729+stephannn@users.noreply.github.com>
2023-08-28 20:10:39 +0200

    Delete create_Autopilot_FirstLogin.cmd

--- create_Autopilot_FirstLogin.cmd
+++
@@ -1,6 +0,0 @@
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle" Product_WindowsAutopilot_FirstLogin.wxs -o obj\ 
-REM -arch x64
-
-"C:\Program Files (x86)\WiX Toolset v3.11\bin\light" obj\Product_WindowsAutopilot_FirstLogin.wixobj -o bin\WindowsAutopilot_FirstLogin.msi -ext WixUIExtension
-
-PAUSE
